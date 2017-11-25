8fd934bba900dbd1890b2b1b1889835f4b0208d0
Janice Darikho <janicedarikho@gmail.com>
2017-11-25 07:42:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ function deleteFromObjectByKey(object, key) {
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  return delete recipes.prop;
+  return delete object.key;
 }
+
+destructivelyDeleteFromObjectByKey(recipes, 'prop');
 
