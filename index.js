f4e1a238ffb3c68ac0b3c73a0d01d4badac8caeb
Janice Darikho <janicedarikho@gmail.com>
2017-11-25 07:29:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,16 +8,23 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  var recipes = {
+    prop: 1
+  };
   recipes['prop2'] = 2;
   return recipes;
 }
 
 function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign( {}, recipes, { prop2 : 2} );
   delete newObj.prop;
   return newObj;
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
+  var recipes = {
+    prop: 1
+  };
   delete recipes.prop;
   return recipes;
 }
